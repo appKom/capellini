cacc23ee13323f134fbdc11c31577e02c01cdbf6
Jørgen Galdal <jorgen.4@live.no>
2025-10-19 13:30:00 +0200

    Update to new ow api (#394)
    * checkpoint
    
    * fix: Changed to correct http payload format
    
    * fix: add types

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,24 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
+import SuperJSON from "superjson";
 
 const adminEmails = process.env.ADMIN_EMAILS?.split(",") || [];
+
+interface User {
+  id: string;
+  phone: string;
+  name: string;
+}
+
+interface Group {
+  type: string;
+  slug: string;
+  abbreviation: string;
+  name: string;
+}
+
+// TODO: Move to config file
+const API_BASE_URL = "https://rpc.online.ntnu.no/api/trpc";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -20,45 +37,61 @@ export const authOptions: NextAuthOptions = {
 
       async profile(profile, tokens) {
         try {
-          const apiUrl = "https://old.online.ntnu.no/api/v1/profile/";
-
           const headers = {
             Authorization: `Bearer ${tokens.access_token}`,
           };
 
-          const response = await fetch(apiUrl, { headers });
-
-          if (!response.ok) {
+          const userUrl = `${API_BASE_URL}/user.getMe`;
+          const userResponse = await fetch(userUrl, { headers });
+          if (!userResponse.ok) {
             throw new Error("Failed to fetch user profile");
           }
+          // TODO: Ensure type safety
+          const userInfoSerialized = await userResponse.json();
+          const userInfo: User = SuperJSON.parse(
+            JSON.stringify(userInfoSerialized.result.data)
+          );
 
-          const userInfo = await response.json();
+          // Check if user is committee
+          const isStaffResponse = await fetch(`${API_BASE_URL}/user.isStaff`, {
+            headers,
+          });
+          if (!isStaffResponse.ok)
+            throw new Error("Failed to fetch staff status");
+          const isCommittee = await isStaffResponse.json();
 
-          const commiteeUrl = `https://old.online.ntnu.no/api/v1/group/online-groups/?members__user=${userInfo.id}`;
-          const committeeResponse = await fetch(commiteeUrl, { headers });
-          if (!committeeResponse.ok)
+          // Get committees of user
+          const commiteeUrl = `${API_BASE_URL}/group.allByMember?input=${encodeURIComponent(
+            SuperJSON.stringify(userInfo.id)
+          )}`;
+
+          const committeeResponse = await fetch(commiteeUrl, {
+            method: "GET", // GET works for read queries
+            headers: { "content-type": "application/json", ...headers },
+          });
+
+          if (!committeeResponse.ok) {
             throw new Error("Failed to fetch committees");
+          }
 
-          const committeeData = await committeeResponse.json();
+          const committeeData: Group[] = SuperJSON.parse(
+            JSON.stringify((await committeeResponse.json()).result.data)
+          );
 
-          // const committees = committee.results.map((committee: any) => ({
-          //   name: committee.name_short,
-          //   id: committee.id,
-          // }));
-
-          // console.log(committee);
+          // TODO: Ta med komité-id (finnes det i det hele tatt?)
+          const committees = committeeData.map(
+            (committee: Group) => committee.slug
+          );
 
           return {
             id: userInfo.id,
             subId: profile.sub,
-            name: userInfo.first_name + " " + userInfo.last_name,
-            email: userInfo.email,
+            name: userInfo.name,
+            email: profile.email,
             //phone: userInfo.phone_number,
             //grade: userInfo.year,
-            committees: committeeData.results.map((committee: any) =>
-              committee.name_short.toLowerCase()
-            ),
-            isCommittee: userInfo.is_committee,
+            committees: committees,
+            isCommittee: isCommittee,
           };
         } catch (error) {
           console.error(error);
